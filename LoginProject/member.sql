fec5f319f99884c28da856247905704719a44528
alsoscope <44256670+alsoscope@users.noreply.github.com>
2018-11-12 01:11:46 +0900

    Update member.sql

--- LoginProject/member.sql
+++ LoginProject/member.sql
@@ -1,6 +1,6 @@
-CREATE TABLE users(id VARCHAR(12), passwd VARCHAR(12), addr VARCHAR(50), age INT(20), email VARCHAR(30), gender	VARCHAR(10),
-name VARCHAR(12), nation VARCHAR(16),PRIMARY KEY (id));
+CREATE TABLE users(id VARCHAR(12), passwd VARCHAR(12), addr VARCHAR(50), age INT(20), email VARCHAR(30), 
+             gender	VARCHAR(10), name VARCHAR(12), nation VARCHAR(16),PRIMARY KEY (id));
 
-insert into users values('java', 'java', '¼­¿ï½Ã', 40, 'aaa@aaa.aa', 'F', 'ÀÌÀÌÀÌ', '´ëÇÑ¹Î±¹');
+insert into users values('java', 'java', 'Â¼Â­Â¿Ã¯Â½Ãƒ', 40, 'aaa@aaa.aa', 'F', 'Ã€ÃŒÃ€ÃŒÃ€ÃŒ', 'Â´Ã«Ã‡Ã‘Â¹ÃŽÂ±Â¹');
 
 commit;
